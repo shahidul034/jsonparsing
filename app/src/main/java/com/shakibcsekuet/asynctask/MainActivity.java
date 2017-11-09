928ba80bfc98ac16966d823460d7e8ee0dd95ee0
SHAKIB <shakibkhanrock@gmail.com>
2017-11-09 17:40:15 +0600

    changed

--- app/src/main/java/com/shakibcsekuet/asynctask/MainActivity.java
+++ app/src/main/java/com/shakibcsekuet/asynctask/MainActivity.java
@@ -9,6 +9,19 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends Activity implements OnClickListener {
 
     Button btn;
@@ -24,28 +37,84 @@ public class MainActivity extends Activity implements OnClickListener {
     }
 
     public void onClick(View view) {
+        String str="https://api.androidhive.info/contacts/";
         // detect the view that was "clicked"
-        switch (view.getId()) {
-            case R.id.button:
-                new Mytask().execute("");
-                break;
-        }
+        Mytask mytask=new Mytask();
+        mytask.execute(str);
     }
 
     private class Mytask extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... params) {
-            for (int i = 0; i < 5; i++) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    Thread.interrupted();
-                }
-                publishProgress(i+"");
-
+            String jsonfeed="";
+            URL url= null;
+            try {
+                url = new URL(params[0]);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
             }
-            return "Executed";
+            HttpsURLConnection conn= null;
+            try {
+                conn = (HttpsURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            InputStream is=null;
+            try {
+                 is=conn.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            BufferedReader br= null;
+            try {
+                br = new BufferedReader(new InputStreamReader(url.openStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String line="";
+            while(line!=null){
+                try {
+                    line=br.readLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                jsonfeed+=line;
+            }
+            JSONObject job=new JSONObject();
+            JSONArray jarr = null;
+            try {
+                 jarr=job.getJSONArray("contacts");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONObject job2= new JSONObject();
+            try {
+                job2 = jarr.getJSONObject(0);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String name="";
+            try {
+                name=job2.getString("name");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONObject job3= null;
+            try {
+                job3 = job2.getJSONObject("phone");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String mob="";
+            try {
+                mob=job3.getString("mobile");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            String s=name+" "+mob;
+            return s;
         }
 
         @Override
